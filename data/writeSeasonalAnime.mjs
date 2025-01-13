74f8ea0782c829c442a5662a6ea48b486b5b5b5e
Oliver Shi <shishishi123@gmail.com>
2025-01-13 16:14:21 -0500

    adding updated code before rerunning as a test

--- data/writeSeasonalAnime.mjs
+++ data/writeSeasonalAnime.mjs
@@ -3,31 +3,37 @@ import fs from 'fs';
 import resolvePath from './resolvePath.mjs';
 import potentialAnime from './potentialAnime.json' with { type: "json" };
 
-function warn(...msg) {
-  // console.warn(...msg)
-}
-
 export default function writeSeasonalAnime() {
   let animeCount = 0
   const yearToSeasonToAnime = {}
+  let hentaiCount = 0
+  let smallMembership = 0
+  let tooShort = 0
   for (const p of potentialAnime) {
-    const { year, season } = p.animeSeason
     const jikanDataPath = getJikanDataFilepath(p.malId);
-    if (p.tags.includes('adult audience only') || p.tags.includes('hentai') || !fs.existsSync(jikanDataPath)) {
-      warn('Skipping case 1', p.title)
+    if (!fs.existsSync(jikanDataPath)) {
+      continue;
+    }
+    const { year, season } = p.animeSeason
+    if (p.tags.includes('adult audience only') || p.tags.includes('hentai') || ) {
+      hentaiCount++;
       continue;
     }
     const jikanData = JSON.parse(fs.readFileSync(jikanDataPath, 'utf-8')).data
-    if (jikanData.members < 2000 || jikanData.score === null) {
-      warn('Skipping case 2', p.title)
+    if (jikanData.score === null) {
+      continue;
+    }
+    if (jikanData.members < 2000) {
+      smallMembership++;
       continue
     }
-    if (!jikanData.duration.includes(" min")) {
-      warn('Skipping case 3', p.title)
-      continue
-    }
-    if (!jikanData.duration.includes('hr') && parseInt(jikanData.duration.split(" min")[0]) < 19) {
-      warn('Skipping case 4', p.title, jikanData.duration.split(" min")[0] )
+    const isOverAnHour = jikanData.duration.includes(" hr")
+    // This should result in the same data as the old code,
+    // commit, then rerun and check git history to verify that data has not changed
+    // Afterwords, move threshold down to at least 10 minutes. Isekai quartet is 11 minutes.
+    const isAtLeast19Minutes = isOverAnHour || (jikanData.duration.includes(" min") && parseInt(jikanData.duration.split(" min")[0]) >= 19)
+    if (!isAtLeast19Minutes) {
+      tooShort++
       continue
     }
     if (!yearToSeasonToAnime[year]) {
@@ -57,4 +63,7 @@ export default function writeSeasonalAnime() {
   fs.writeFileSync(resolvePath('seasonalAnime.json'), JSON.stringify(yearToSeasonToAnime, null, 2))
   fs.writeFileSync(resolvePath('seasonalAnime.min.json'), JSON.stringify(yearToSeasonToAnime))
   console.log('anime count! -', animeCount, 'pruned - ', potentialAnime.length - animeCount)
+  console.log('hentai count:', hentaiCount)
+  console.log('shows with less than 2000 members', smallMembership)
+  console.log('shorts with episodes under 19 minutes', tooShort)
 }
